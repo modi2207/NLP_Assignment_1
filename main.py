f7ceb1368a826e6a4a14daab9fe3bdc6bcb2963c
modi2207 <modichirag2207@gmail.com>
2023-08-23 18:23:21 +0530

    Replaced 0 comments files

--- main.py
+++ main.py
@@ -29,17 +29,19 @@ class ReditScrape:
     def generate(self):
 
             subreddit = self.reddit.subreddit(self.subreddit)
-            top_posts = subreddit.top(limit=self.total_post, time_filter='all')
+            top_posts = subreddit.top(limit=150, time_filter='all')
             comments_path = os.path.join(os.getcwd(),'Submission\\comments')
 
             self.create_dir(comments_path)
-
+            count=0
             post_df=[]
             for post in top_posts:
                 post_title = post.title
                 comment_df=[]
                 submission=self.reddit.submission(post.id)
                 submission.comments.replace_more(limit=0)
+                if len(submission.comments.list())==0:
+                    continue
                 for comment in submission.comments.list():
                     comment_df.append([comment.id,comment.body,comment.score,comment.depth,datetime.datetime.utcfromtimestamp(comment.created_utc).strftime('%Y-%m-%d %H:%M:%S UTC'),len(comment.replies.list()),comment.author,post.id,comment.parent_id,len(comment.body),comment.edited,comment.is_submitter,comment.subreddit.display_name])
                 comment_df=pd.DataFrame(comment_df, columns=['ID','Text','Upvotes','Depth','Created Timestamp(UTC)','Replies','Author','Post ID','Parrant Comment ID','Text Length','Edited','Is Submitter','Subreddit Name'])
@@ -50,6 +52,9 @@ class ReditScrape:
                 f.close()
                 post_df.append([post.title,post.id,post.url,post.score,post.num_comments,datetime.datetime.utcfromtimestamp(post.created_utc).strftime('%Y-%m-%d %H:%M:%S UTC'),post.subreddit.display_name,post.author,post.selftext,post.upvote_ratio,len(post.selftext),post.edited])
                 print(f"CSV file created for post '{post.id}'")
+                count+=1
+                if count==self.configurationData["total_post"]:
+                    break
             post_df=pd.DataFrame(post_df,columns=['Title','ID','URL','Upvotes','Comments','Created Timestamp(UTC)','Subreddit Name','Author','Text','Upvotes Ratio','Text Length','Edited'])
             csv_filename = f"posts.csv"
             csv_path = os.path.join(os.path.join(os.getcwd(),'Submission'), csv_filename)
