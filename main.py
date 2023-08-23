c943a4062dacf6d31bb3f5bb358806308dd98662
modi2207 <modichirag2207@gmail.com>
2023-08-23 12:43:07 +0530

    Added some fields in comments and posts

--- main.py
+++ main.py
@@ -41,16 +41,16 @@ class ReditScrape:
                 submission=self.reddit.submission(post.id)
                 submission.comments.replace_more(limit=0)
                 for comment in submission.comments.list():
-                    comment_df.append([comment.id,comment.body,comment.score,comment.depth,datetime.datetime.utcfromtimestamp(comment.created_utc).strftime('%Y-%m-%d %H:%M:%S UTC'),len(comment.replies.list()),comment.author,post.id])
-                comment_df=pd.DataFrame(comment_df, columns=['ID','Text','Upvotes','Depth','Created Timestamp(UTC)','Replies','Author','Post ID'])
+                    comment_df.append([comment.id,comment.body,comment.score,comment.depth,datetime.datetime.utcfromtimestamp(comment.created_utc).strftime('%Y-%m-%d %H:%M:%S UTC'),len(comment.replies.list()),comment.author,post.id,comment.parent_id,len(comment.body),comment.edited,comment.is_submitter,comment.subreddit.display_name])
+                comment_df=pd.DataFrame(comment_df, columns=['ID','Text','Upvotes','Depth','Created Timestamp(UTC)','Replies','Author','Post ID','Parrant Comment ID','Text Length','Edited','Is Submitter','Subreddit Name'])
                 csv_filename = f"{post.id}.csv"
                 csv_path = os.path.join(comments_path, csv_filename)
                 f = open(csv_path, "w")
                 comment_df.to_csv(csv_path, index=True)
                 f.close()
-                post_df.append([post.title,post.id,post.url,post.score,post.num_comments,datetime.datetime.utcfromtimestamp(post.created_utc).strftime('%Y-%m-%d %H:%M:%S UTC'),post.subreddit.display_name,post.author,post.selftext])
+                post_df.append([post.title,post.id,post.url,post.score,post.num_comments,datetime.datetime.utcfromtimestamp(post.created_utc).strftime('%Y-%m-%d %H:%M:%S UTC'),post.subreddit.display_name,post.author,post.selftext,post.upvote_ratio,len(post.selftext),post.edited])
                 print(f"CSV file created for post '{post.id}'")
-            post_df=pd.DataFrame(post_df,columns=['Title','ID','URL','Upvotes','Comments','Created Timestamp(UTC)','Subreddit Name','Author','Text'])
+            post_df=pd.DataFrame(post_df,columns=['Title','ID','URL','Upvotes','Comments','Created Timestamp(UTC)','Subreddit Name','Author','Text','Upvotes Ratio','Text Length','Edited'])
             csv_filename = f"posts.csv"
             csv_path = os.path.join(os.path.join(os.getcwd(),'Submission'), csv_filename)
             f = open(csv_path, "w")
@@ -67,7 +67,7 @@ class ReditScrape:
 
 redit_instance=ReditScrape()
 redit_instance.initialize()
-#redit_instance.generate()
+redit_instance.generate()
 redit_instance.merge()
 
 
